2402fa7ef95522466256e8b9bc589c2f7e2537eb
Yogesh R <116285581+Yogesh73151915059@users.noreply.github.com>
2023-07-07 21:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
  let team=players;
- let team1=[players]
- let cap1={person}
+ let team1=[...players]
+ let cap1={...person}
    console.log(team)
    console.log(team1)
    console.log(cap1)
